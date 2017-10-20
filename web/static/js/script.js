014e4d35b711d77a67f99eccc7d7f1f74d8a71d0
Jorge <jorgemsm12316@gmail.com>
2017-10-20 09:44:29 -0500

    cambios

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -2,15 +2,15 @@
     $("#registro").popover({
       placement: 'bottom',
       html: true,
-      title : '<h6>Como</h6>',
-      content : '<button type="button" class="btn btn-danger btn-sm" id="admReg">Administrador</button> <button type="button" id="egresadoReg" class="btn btn-success btn-sm">Egresado</button>'
+      title : '<h6>Como</h6>', 
+      content : '<button type="livedemo" class="btn btn-primary btn-lg" id="admReg">Administrador</button> <button type="button" id="egresadoReg" class="btn btn-success btn-sm">Egresado</button>'
     })
     $('html').click(function() {
       $('#close').popover('hide');
     });
   });
 
-$('body').on('click', 'button#admReg', function() {
+$('body').on('click', 'button#1admReg', function() {
     $(location).attr('href','pre_registro_admin');
 });
 $('body').on('click', 'button#egresadoReg', function() {
