44764612fb323a64a42446b840101bc777dedc63
Jorge <jorgemsm12316@gmail.com>
2017-10-20 22:46:03 -0500

    cambios pull

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -2,7 +2,6 @@
     $("#registro").popover({
       placement: 'bottom',
       html: true,
-      title : '<h6>Como</h6>', 
       content : '<button type="button" class="btn btn-danger btn-sm" id="admReg">Administrador</button> <button type="button" id="egresadoReg" class="btn btn-success btn-sm">Egresado</button>'
     })
     $('html').click(function() {
@@ -10,10 +9,28 @@
     });
   });
 
-$('body').on('click', 'button#1admReg', function() {
+$(function(){
+    $("#registro2").popover({
+      placement: 'bottom',
+      html: true,
+      content : '<button type="button" class="btn btn-danger btn-sm" id="admReg2">Administrador</button> <button type="button" id="egresadoReg2" class="btn btn-success btn-sm">Egresado</button>'
+    })
+    $('html').click(function() {
+      $('#close').popover('hide');
+    });
+});
+
+$('body').on('click', 'button#admReg', function() {
     $(location).attr('href','pre_registro_admin');
 });
 $('body').on('click', 'button#egresadoReg', function() {
+    $(location).attr('href','pre_registro_egresado');
+});
+
+$('body').on('click', 'button#admReg2', function() {
+    $(location).attr('href','pre_registro_admin');
+});
+$('body').on('click', 'button#egresadoReg2', function() {
     $(location).attr('href','pre_registro_egresado');
 });
 
@@ -21,8 +38,18 @@ $(function(){
     $("#ingreso").popover({
       placement: 'bottom',
       html: true,
-      title : '<h6>Como</h6>',
       content : '<button type="button" class="btn btn-danger btn-sm" id="admIng">Administrador</button> <button type="button" id="egresadoIng" class="btn btn-success btn-sm">Egresado</button>'
+    })
+    $('html').click(function() {
+      $('#close').popover('hide');
+    });
+  });
+
+$(function(){
+    $("#ingreso2").popover({
+      placement: 'bottom',
+      html: true,
+      content : '<button type="button" class="btn btn-danger btn-sm" id="admIng2">Administrador</button> <button type="button" id="egresadoIng2" class="btn btn-success btn-sm">Egresado</button>'
     })
     $('html').click(function() {
       $('#close').popover('hide');
@@ -32,5 +59,12 @@ $('body').on('click', 'button#admIng', function() {
     $(location).attr('href','login_admin');
 });
 $('body').on('click', 'button#egresadoIng', function() {
+    $(location).attr('href','login_egresado');
+});
+
+$('body').on('click', 'button#admIng2', function() {
+    $(location).attr('href','login_admin');
+});
+$('body').on('click', 'button#egresadoIng2', function() {
     $(location).attr('href','login_egresado');
 });
