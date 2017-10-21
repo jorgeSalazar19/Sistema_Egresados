a02fa740a15922c38c7d3a931e64c4b531faec1a
Jorge <jorgemsm12316@gmail.com>
2017-10-21 10:34:50 -0500

    cambios

--- web/static/js/script.js
+++ web/static/js/script.js
@@ -20,17 +20,17 @@ $(function(){
   $("#registro2").popover({
     placement: 'bottom',
     html: true,
-    content : '<button type="button" class="btn btn-danger btn-sm" id="admReg" style=" background-color: #3a906d;" >Administrador</button> <button type="button" id="egresadoReg" style=" background-color: #3a906d;" class="btn btn-success btn-sm">Egresado</button>'
+    content : '<button type="button" class="btn btn-danger btn-sm" id="admReg2" style=" background-color: #3a906d;" >Administrador</button> <button type="button" id="egresadoReg2" style=" background-color: #3a906d;" class="btn btn-success btn-sm">Egresado</button>'
   })
   $('html').click(function() {
     $('#close').popover('hide');
   });
 });
 
-$('body').on('click', 'button#admReg', function() {
+$('body').on('click', 'button#admReg2', function() {
     $(location).attr('href','pre_registro_admin');
 });
-$('body').on('click', 'button#egresadoReg', function() {
+$('body').on('click', 'button#egresadoReg2', function() {
     $(location).attr('href','pre_registro_egresado');
 });
 
@@ -55,7 +55,7 @@ $(function(){
     $("#ingreso2").popover({
       placement: 'bottom',
       html: true,
-      content : '<button type="button" class="btn btn-danger btn-sm" id="admIng" style=" background-color: #3a906d;" >Administrador</button> <button type="button" id="egresadoIng" style=" background-color: #3a906d;" class="btn btn-success btn-sm">Egresado</button>'
+      content : '<button type="button" class="btn btn-danger btn-sm" id="admIng2" style=" background-color: #3a906d;" >Administrador</button> <button type="button" id="egresadoIng2" style=" background-color: #3a906d;" class="btn btn-success btn-sm">Egresado</button>'
     })
     $('html').click(function() {
       $('#close').popover('hide');
