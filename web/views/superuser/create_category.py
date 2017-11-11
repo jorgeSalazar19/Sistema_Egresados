f426c726a410da2cd8923e619aeb87b872160375
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2017-11-11 15:24:49 -0500

    funcionalidad crear categoria

--- web/views/superuser/create_category.py
+++ web/views/superuser/create_category.py
@@ -1,12 +1,13 @@
 from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
+from web.forms import CreateFormCategory
 
 from domain.models import Category
 from django.contrib.auth.models import User
 
 def CreateCategory(request):
-
+    mensaje = (False,'')
     if request.method == 'GET':
         username = request.GET.get('username')
         usuario = User.objects.filter(username=username)
@@ -16,13 +17,27 @@ def CreateCategory(request):
         username = request.GET.get('username')
         usuario = User.objects.filter(username=username)
         usuario = usuario[0]
-        nombre_categoria = request.POST.get('name_category')
-        descripcion_categoria = request.POST.get('description_category')
+        form_category = CreateFormCategory(data=request.POST)
+
+        if form_category.is_valid():
+            name = form_category.cleaned_data['name']
+            description = form_category.cleaned_data['description']
+            form_category.save()
+            mensaje = (True , "Categoria guardada full")
+        else:
+            errors = form_category.get_errors()
+            message_e = []
+            for error in errors:
+                message_e.append(str(form_category.errors[error]))
+            mensaje = (True , message_e)
+            errors = form_category.clean_errors()
+            mensaje = (True , message_e)
+
     
-    error = (False,'')
+
     template = loader.get_template('Root/CrearCategoria.html')
     ctx = {
-            'error' : error,
+            'mensaje' : mensaje,
             'usuario' : usuario,
     }
     return HttpResponse(template.render(ctx,request))
