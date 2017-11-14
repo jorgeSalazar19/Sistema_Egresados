99cc6e4e8b7dca089eaed277d3978c8a85157d42
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2017-11-14 15:29:09 -0500

    merge

--- web/views/graduated/register_done.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import redirect
-from django.http import HttpResponse
-from django.template import loader
-
-def RegisterDone(request):
-    template = loader.get_template('Egresado/registroExitoso.html')
-    ctx = { 
-    }   
-    return HttpResponse(template.render(ctx,request))
