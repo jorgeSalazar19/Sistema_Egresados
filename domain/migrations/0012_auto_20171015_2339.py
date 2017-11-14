fd0566a599a1f3ec26ea1ba32c1c707fee25d9c5
jorgesalazar19 <jorgemsm12316@utp.edu.co>
2017-11-14 15:09:25 -0500

    merge

--- domain/migrations/0012_auto_20171015_2339.py
+++ domain/migrations/0012_auto_20171015_2339.py
@@ -8,7 +8,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('domain', '0010_graduated_fisrt_login'),
+        ('domain', '0009_auto_20171111_1929'),
     ]
 
     operations = [
